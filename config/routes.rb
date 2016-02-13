0405d8671794263d9dd321b6763b8faf49b16410
Andrew <strymok89@gmail.com>
2016-02-13 16:09:20 +0200

    orders ready

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :pets do
     get :my, on: :collection
   end
+
+  resources :orders do
+    get :my, on: :collection
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
